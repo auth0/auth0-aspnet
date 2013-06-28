b3193e3fbfae44b7ec7b7ce959edd223ea4df91e
siacomuzzi <sebastian.iacomuzzi@gmail.com>
2013-06-28 14:43:05 -0300

    update to ClaimsCookie (>= 0.9.7)

--- content/net45/LoginCallback.ashx.cs.pp
+++ content/net45/LoginCallback.ashx.cs.pp
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
-using System.Configuration;
 
 namespace $rootnamespace$
 {
@@ -18,29 +17,28 @@ namespace $rootnamespace$
             var token = client.ExchangeAuthorizationCodePerAccessToken(context.Request.QueryString["code"], ConfigurationManager.AppSettings["auth0:CallbackUrl"]);
             var profile = client.GetUserInfo(token.AccessToken);
 
-            var user = new Dictionary<string, string>
+            var user = new List<KeyValuePair<string, object>>
             {
-                { "name", profile.Name },
-                { "email", profile.Email },
-                { "family_name", profile.FamilyName },
-                { "gender", profile.Gender },
-                { "given_name", profile.GivenName },
-                { "nickname", profile.Nickname },
-                { "picture", profile.Picture },
-                { "user_id", profile.UserId },
-                { "id_token", token.IdToken },
-                { "connection", profile.Identities.First().Connection },
-                { "provider", profile.Identities.First().Provider },
-				{ "access_token", token.AccessToken }
+                new KeyValuePair<string, object>("name", profile.Name),
+                new KeyValuePair<string, object>("email", profile.Email),
+                new KeyValuePair<string, object>("family_name", profile.FamilyName),
+                new KeyValuePair<string, object>("gender", profile.Gender),
+                new KeyValuePair<string, object>("given_name", profile.GivenName),
+                new KeyValuePair<string, object>("nickname", profile.Nickname),
+                new KeyValuePair<string, object>("picture", profile.Picture),
+                new KeyValuePair<string, object>("user_id", profile.UserId),
+                new KeyValuePair<string, object>("id_token", token.IdToken),
+                new KeyValuePair<string, object>("connection", profile.Identities.First().Connection),
+                new KeyValuePair<string, object>("provider", profile.Identities.First().Provider)
             };
-			
-			// NOTE: Uncomment the following code in order to include claims from associated identities
-            // profile.Identities.ToList().ForEach(i =>
-            // {
-            //     user.Add(i.Connection + ".access_token", i.AccessToken);
-            //     user.Add(i.Connection + ".provider", i.Provider);
-            //     user.Add(i.Connection + ".user_id", i.UserId);
-            // });
+
+            // NOTE: Uncomment the following code in order to include claims from associated identities
+            //profile.Identities.ToList().ForEach(i =>
+            //{
+            //    new KeyValuePair<string, object>(i.Connection + ".access_token", i.AccessToken);
+            //    new KeyValuePair<string, object>(i.Connection + ".provider", i.Provider);
+            //    new KeyValuePair<string, object>(i.Connection + ".user_id", i.UserId);
+            //});
 
             // NOTE: this will set a cookie with all the user claims that will be converted 
             //       to a ClaimsPrincipal for each request using the ClaimsCookie HttpModule . 
